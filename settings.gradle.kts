d3ff702a7f1f31a4b0b013f8e96ec35ae19ecaac
kaioc <kaioplandel@gmail.com>
2022-12-07 19:43:31 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.plandell.ktor-movie-server"
---
+++ src/main/kotlin/com/plandell/Application.kt
@@ -0,0 +1,14 @@
+package com.plandell
+
+import io.ktor.server.application.*
+import com.plandell.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
